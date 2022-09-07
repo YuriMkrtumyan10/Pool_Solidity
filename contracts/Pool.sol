903f2e414082db023596e2974ae04f1f516e33ae
Yuri <yurimkrtumyan02@gmail.com>
2022-09-08 02:49:14 +0400

    SSH commit

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -3,20 +3,17 @@ pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./PoolToken.sol";
+import "./IRouter.sol";
 
 contract Pool {
-    uint256 pTokenAmount;
+    uint256 LpTokenAmount;
     IERC20 public stable;
-    PoolToken public pToken;
+    PoolToken public lpToken;
 
-    constructor(address _stable, address _pToken) {
+    constructor(address _stable, address _lpToken) {
         stable = IERC20(_stable);
-        pToken = PoolToken(_pToken);
+        lpToken = PoolToken(_lpToken);
     }
-
-    // function contractProfit() public {
-    //     stable.mint(address(this), 1000);
-    // }
 
     function deposit(uint256 amount, address tokenAddress) external payable {
         require(
@@ -33,18 +30,57 @@ contract Pool {
             "Pool: Not enough allowance"
         );
         stable.transferFrom(msg.sender, address(this), amount);
-        pTokenAmount += amount / 10;
-        pToken.mint(msg.sender, amount / 10);
+        LpTokenAmount += amount / 10;
+        lpToken.mint(msg.sender, amount / 10);
     }
 
-    function withdraw() external {
-        require(pToken.balanceOf(msg.sender) > 0, "Pool: Dont have pTokens");
-        uint256 pTokenBalance = pToken.balanceOf(msg.sender);
-        pToken.burn(msg.sender, pTokenBalance);
-        stable.transfer(
-            msg.sender,
-            ((pTokenBalance * (stable.balanceOf(address(this)))) / pTokenAmount)
+    function withdraw(uint256 _amount) external {
+        require(lpToken.balanceOf(msg.sender) > 0, "Pool: Dont have LpTokens");
+        uint256 lpValueOfaGivenAmount = _amount / 10;
+        require(
+            lpToken.balanceOf(msg.sender) - lpValueOfaGivenAmount >= 0,
+            "Pool: Not enough LpTokens"
         );
-        pTokenAmount -= pTokenBalance;
+        lpToken.burn(msg.sender, lpValueOfaGivenAmount);
+        stable.transfer(msg.sender, _amount);
+
+        LpTokenAmount -= lpValueOfaGivenAmount;
+    } 
+
+    function arbitrage(
+        address _router1,
+        address _router2,
+        address _token1,
+        address _token2,
+        uint256 _amount
+    ) external {
+        swap(_amount, _token1, _token2, _router1);
+        uint256 token2BalanceBefore = IERC20(_token2).balanceOf(address(this));
+        uint256 token2BalanceAfter = IERC20(_token2).balanceOf(address(this)) -
+            token2BalanceBefore;
+        swap(token2BalanceAfter, _token2, _token1, _router2);
+    }
+
+    function swap(
+        uint256 _amount,
+        address _token1,
+        address _token2,
+        address _router
+    ) private {
+        address[] memory tokens = new address[](2);
+        tokens[0] = _token1;
+        tokens[1] = _token2;
+        uint256 desirableAmount = IRouter(_router).getAmountsOut(
+            _amount,
+            tokens
+        )[1];
+        require(desirableAmount > 100);
+        IRouter(_router).swapExactTokensForTokens(
+            _amount,
+            1,
+            tokens,
+            msg.sender,
+            block.timestamp + 300
+        );
     }
 }
